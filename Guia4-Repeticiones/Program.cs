55c3f3ad4d9b20691ffb8882d85fed645df8ea4c
MatiBravo47 <matibravoneron@gmail.com>
2024-05-23 21:12:50 -0300

    Agregar archivos de proyecto.

---
+++ Guia4-Repeticiones/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia4_Repeticiones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            1. Numeros correlativos del 1 hasta el ingresado:
+            Objetivo: Escribir un programa que solicite la carga de un valor positivo 
+            y nos muestre desde 1 hasta el valor ingresado de uno en uno
+            */
+            /*
+            int valor;
+            int contador = 1;
+            Console.WriteLine("Ingrese un numero");
+            valor = int.Parse(Console.ReadLine()); 
+            while (contador <= valor) { 
+                Console.WriteLine(contador);
+                contador++;
+            }
+            Console.ReadLine(); 
+            */
+        }
+    }
+}
